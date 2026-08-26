7a1914c9e4311c1d1ad794cfdf7211de374e0e2a
Carddera1842 <carddera1842@gmail.com>
2026-08-26 15:18:05 -0500

    Added card play endpoint!

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,53 @@
 from fastapi import FastAPI
 
+from card import Card
+from enemy import Enemy
+from player import Player
+
 app = FastAPI()
+
+player = Player("Survivor", 100, 5)
+infected = Enemy("Infected", 100, 6)
+handgun = Card("Handgun", 8, 5)
+
+def get_game_state():
+    return {
+        "player": {
+            "name": player.name,
+            "health": player.health,
+            "energy": player.energy
+        },
+        "enemy": {
+            "name": infected.name,
+            "health": infected.health,
+            "attack": infected.attack
+        },
+        "card": {
+            "name": handgun.name,
+            "damage": handgun.damage,
+            "cost": handgun.cost
+        }
+    }
 
 @app.get("/api/game")
 def  get_game():
-    return {
-        "player": {
-            "name": "Survivor",
-            "health": 100,
-            "energy": 50
-        },
-        "enemy": {
-            "name": "Infected",
-            "health": 100,
-            "attack": 6
+    return get_game_state()
+
+@app.post("/api/game/play-card")
+def play_card():
+    if player.energy >= handgun.cost:
+        player.energy -= handgun.cost
+        infected.health -= handgun.damage
+
+        return {
+            "success": True,
+            "message": f"You played {handgun.name}!",
+            "game": get_game_state()
         }
-    }
+    else:
+        return {
+            "success": False,
+            "message": "Not enough energy to play the card.",
+            "game": get_game_state()
+        }
 
