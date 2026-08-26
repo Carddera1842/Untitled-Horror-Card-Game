d21ec62b0b33d2b8050620b0410f8f826d8aa2aa
Carddera1842 <carddera1842@gmail.com>
2026-08-26 14:24:55 -0500

    Setup for frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <main>
+      <h1>Untitled Horror Card Game</h1>
+
+      <section>
+        <h2>Survivor</h2>
+        <p>Health: 100</p>
+        <p>Energy: 50</p>
+      </section>
+
+      <section>
+        <h2>Infected</h2>
+        <p>Health: 100</p>
+        <p>Attack: 6</p>
+      </section>
+    </main>
+  )
+}
+
+export default App
