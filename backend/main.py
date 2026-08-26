8c08320dfa29512b4c5316d82c9771aaa009e1b9
Carddera1842 <carddera1842@gmail.com>
2026-08-26 16:31:42 -0500

    Add multiple cards and card selction

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from card import Card
 from enemy import Enemy
 from player import Player
 
+class PlayCardRequest(BaseModel):
+    card_index: int
+
 app = FastAPI()
 
 player = Player("Survivor", 100, 5)
 infected = Enemy("Infected", 100, 6)
-handgun = Card("Handgun", 8, 5)
+hand = [
+    Card("Handgun", 8, 2),
+    Card("Combat Knife", 4, 1),
+    Card("Shotgun", 15, 3)
+]
 
 def get_game_state():
     return {
@@ -22,11 +30,13 @@ def get_game_state():
             "health": infected.health,
             "attack": infected.attack
         },
-        "card": {
-            "name": handgun.name,
-            "damage": handgun.damage,
-            "cost": handgun.cost
+        "hand": [{
+            "name": card.name,
+            "damage": card.damage,
+            "cost": card.cost
         }
+        for card in hand
+        ]
     }
 
 @app.get("/api/game")
@@ -34,14 +44,16 @@ def  get_game():
     return get_game_state()
 
 @app.post("/api/game/play-card")
-def play_card():
-    if player.energy >= handgun.cost:
-        player.energy -= handgun.cost
-        infected.health -= handgun.damage
+def play_card(request: PlayCardRequest):
+    card = hand[request.card_index]
+
+    if player.energy >= card.cost:
+        player.energy -= card.cost
+        infected.health -= card.damage
 
         return {
             "success": True,
-            "message": f"You played {handgun.name}!",
+            "message": f"You played {card.name}!",
             "game": get_game_state()
         }
     else:
